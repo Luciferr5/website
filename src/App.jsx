d60d9349cb362ac0eefb6fb38847826046b3c978
Luciferr5 <saxenp4@mcmaster.ca>
2025-05-22 23:40:00 +0100

    fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import {LoadingScreen} from "./components/LoadingScreen";
 import {Navbar} from "./components/Navbar";
 import {MobileMenu} from "./components/MobileMenu";
+// import {Home} from "./components/sections/Home";
 import {Home} from "./components/sections/Home";
 import {About} from "./components/sections/About"
 import {Projects} from "./components/sections/Projects"
