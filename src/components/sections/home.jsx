60b5900f549f40cc315a832c71d9a784a847b511
Luciferr5 <saxenp4@mcmaster.ca>
2025-05-23 11:18:37 +0100

    updates

--- src/components/sections/home.jsx
+++ src/components/sections/home.jsx
@@ -9,13 +9,11 @@ export const Home = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
-            Hi, I'm Praksy Saxy
+            Prakhar Saxena 
           </h1>
 
-          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            I’m a full-stack developer who loves crafting clean, scalable web
-            applications. My goal is to build solutions that offer both
-            exceptional performance and a delightful user experience.
+          <p className="tex-gray-400 text-3xl mb-8 max-w-lg mx-auto">
+            Engineering the future.
           </p>
           <div className="flex justify-center space-x-4">
             <a
